2b61a11a01056685c455312e8a09dbe4fc664b57
zippostorm <toghkklli@gmail.com>
2025-05-16 21:26:45 +0300

    Finish project

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import { connectDB } from "./lib/db.js";
 
@@ -15,6 +16,14 @@ app.use(express.json({ limit: "20mb" }));
 app.use(cors());
 
 app.use("/api/superheroes", superheroRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
